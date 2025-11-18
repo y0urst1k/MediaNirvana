ded5271bd0c6605813e65fcbb973f85a0f4eac17
y0urst1k <y0urstyurk@yandex.ru>
2025-11-18 22:11:48 +0300

    feat: adding other modules

---
+++ Dialogs/DialogsModule.cs
@@ -0,0 +1,7 @@
+namespace Dialogs
+{
+    public class DialogsModule
+    {
+
+    }
+}
---
+++ Infrastructure/InfrastructureModue.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.EF.Context;
+using Infrastructure.Interface;
+using Infrastructure.Service;
+
+namespace Infrastructure
+{
+    public class InfrastructureModue : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<MediaNirvanaDbContext>();
+            containerRegistry.Register<IUserDialog, UserDialog>();
+        }
+    }
+}
